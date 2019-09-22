be4846d51a5f556f231398337dfc35ae4eca0c25
MargaritaKhodzka <mkhodzka@gmail.com>
2019-09-21 21:51:56 -0400

    section 4 - return values from a function

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
-// var date = new Date();
-// document.body.innerHTML = "<h1>The date today is " + (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>";
+const multiply = (a, b) => {
+  let result = a * b;
+  console.log(result);
+  return result;
+};
+let multiplied = multiply(3,4);
 
-// var a = 4;
-// var b = 5;
-// var sum = a + b;
-// document.body.innerHTML = sum;
 
-var pens;
-pens = ['red', 'blue', 'green', 'orange'];
-// console.log(pens[3]);
-console.log('Before: ', pens);
-console.log('Array length: ', pens.length);
-pens.reverse();
-// remove the first value of the array
-pens.shift();
-// add new value to the front of the array
-pens.unshift('purple', 'black');
-// remove the last value of the array
-pens.pop();
-// add new value to the end of the array
-pens.push('pink', 'prussian blue');
-// remove item from the middle (pos, n)
-pens.splice(2, 1);
-console.log('After: ', pens);
+const findBiggestFraction = (a, b) => {
+  let result;
+  a > b ? result = ['firstFraction', a] : result = ['secondFraction', b];
+  return result;
+};
 
-// make a copy of the array
-var newPens = pens.slice();
-console.log('New pens: ', newPens);
+let firstFraction = 3/4;
+let secondFraction = 5/7;
+let fractionResult = findBiggestFraction(firstFraction, secondFraction);
 
-var result = pens.indexOf('blue', 1);
-console.log('The index position is: ', result);
-console.log('The value is: ', pens[result]);
-
-var arrayString = pens.join(', ');
-console.log('String from array: ', arrayString);
+console.log('First fraction result: ', firstFraction);
+console.log('Second fraction result: ', secondFraction);
+console.log('Fraction ' + fractionResult[0] + ' with a value of ' + fractionResult[1] + ' is the biggest.');
