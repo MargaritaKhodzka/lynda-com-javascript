a6c4f11d1f5d65b672882b5db0ccb7ab6aa90da6
MargaritaKhodzka <mkhodzka@gmail.com>
2021-02-09 22:53:36 -0500

    build count-up timer

---
+++ README.md
@@ -0,0 +1,1 @@
+# lynda-typing-speed-tester
--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+let timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
+runTimer = () => {
+  let currentTime = timer[0] + ':' + timer[1] + ':' + timer[2];
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
 
+  timer[0] = Math.floor((timer[3]/100)/60);
+  timer[1] = Math.floor((timer[3]/100) - (timer[0]*60));
+  timer[2] = Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
+}
 
 // Match the text entered with the provided text on the page:
 
@@ -21,6 +30,9 @@ spellCheck = () => {
 // Start the timer:
 start = () => {
   let textEnteredLength = testArea.value.length;
+  if (textEnteredLength === 0) {
+    setInterval(runTimer, 10);
+  }
   console.log(textEnteredLength);
 }
 
