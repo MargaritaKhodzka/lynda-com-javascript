b6d8fffa98732bbd61aa3d64f04f2446332b7b8a
MargaritaKhodzka <mkhodzka@gmail.com>
2021-02-09 23:13:09 -0500

    detected soelling error by matching strings

--- script.js
+++ script.js
@@ -29,7 +29,17 @@ runTimer = () => {
 
 spellCheck = () => {
   let textEntered = testArea.value;
-  console.log(textEntered);
+  let originTextMatch = originText.substring(0, textEntered.length);
+
+  if (textEntered == originText) {
+    testWrapper.style.borderColor = '#429890';
+  } else {
+    if (textEntered == originTextMatch) {
+      testWrapper.style.borderColor = '#65CCf3';
+    } else {
+      testWrapper.style.borderColor = '#E95D0F';
+    }
+  }
 }
 
 // Start the timer:
